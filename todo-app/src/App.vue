e235f2f5255400d5611e5e31d0cafbe2f48aa966
Xuqian Zhang <zxuqian@gmail.com>
2020-09-05 12:55:34 +0800

    Initial Commit -  Add HTML Structure and part of CSS

---
+++ todo-app/src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <main>
+    <div class="container">
+      <h1>Feng Todo</h1>
+      <input type="text" name="todo" id class="input-suffix" />
+      <div class="filters">
+        <span class="filter">全部</span>
+        <span class="filter">已完成</span>
+        <span class="filter">未完成</span>
+      </div>
+      <div class="tood-list">
+        <div class="todo-item">
+          <label>
+            <input type="checkbox" name id />
+            Todo 1
+          </label>
+        </div>
+        <div class="todo-item">
+          <label>
+            <input type="checkbox" name id />
+            Todo 2
+          </label>
+        </div>
+        <div class="todo-item">
+          <label>
+            <input type="checkbox" name id />
+            Todo 3
+          </label>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  // components: {
+  //   HelloWorld,
+  // },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Helvetica, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+/* 整个页面 */
+main {
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  align-content: center;
+  justify-content: center;
+}
+
+.container {
+  box-shadow: 0px 0px 24px rgba(0, 0, 0, 0.15);
+  border-radius: 24px;
+  padding: 24px 18px;
+}
+
+/* 标题 */
+h1 {
+  margin: 24px 0;
+  font-size: 24px;
+}
+
+/* 添加框 */
+.input-suffix {
+  border: 1px solid #eeeeee;
+  padding: 10px 8px;
+  border-radius: 8px;
+}
+</style>
