3dabbeade463b6a37f26444711a69623ebc1b53c
Xuqian Zhang <zxuqian@gmail.com>
2020-10-03 12:42:11 +0800

    Add async fetch

--- todo-app/src/composables/useTodos.js
+++ todo-app/src/composables/useTodos.js
@@ -1,9 +1,26 @@
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 
 export default function useTodos() {
   const todos = ref([]);
   // 添加 todo
   const addTodo = (todo) => todos.value.push(todo);
+
+  // 获取远程 todos
+  const fetchTodos = async () => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos?_limit=5"
+    );
+    const rawTodos = await response.json();
+    todos.value = rawTodos.map((todo) => ({
+      id: todo.id,
+      content: todo.title,
+      completed: todo.completed,
+    }));
+  };
+
+  onMounted(() => {
+    fetchTodos();
+  });
 
   return {
     todos,
